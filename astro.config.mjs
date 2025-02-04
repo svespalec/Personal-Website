5d07787ecc7735c302ddb4e9ac1bcbd1721314c8
Sam <vespalecsamuel@gmail.com>
2025-02-03 16:24:31 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,17 @@ import icon from "astro-icon";
 
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  output: 'static',
+  site: 'https://vespalec.com',
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 });
